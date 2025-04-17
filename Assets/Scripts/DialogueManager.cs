5e550f9a2f0ae6756deb9f246c6bc45e42bac00c
Andrea Borghesi <andrea.borghesi@atlantica.it>
2025-04-17 12:01:59 +0200

    Improved dialogue interface

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,10 +12,13 @@ public class DialogueManager : MonoBehaviour
     private GameObject optionPrefab;
     [SerializeField]
     private DialogueContainer dialogue;
+    [SerializeField]
+    private InputActions controls;
     private DialogueNodeData currentNode;
     private TextMeshProUGUI dialogueBoxText;
     private GameObject optionBox;
     private List<GameObject> optionList = new List<GameObject>();
+    private bool optionsActive = false;
     private bool ongoing = false;
     public bool Ongoing
     {
@@ -27,6 +28,8 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueBoxText = dialogueInterface.transform.Find("Text Box").GetComponentInChildren<TextMeshProUGUI>();
         optionBox = dialogueInterface.transform.Find("Option Box").gameObject;
+        dialogueInterface.SetActive(false);
+        optionBox.SetActive(false);
     }
     void Update()
     {
@@ -35,9 +38,6 @@ public class DialogueManager : MonoBehaviour
             switch (currentNode.NodeType)
             {
                 case "SimpleDialogueNode":
-                    if (Input.GetKeyUp(KeyCode.Space))
-                        NextNode(currentNode.OutputPortList[0].Guid);
-                    break;
                 case "ChoiceDialogueNode":
                     break;
                 case "BooleanNode":
@@ -55,17 +55,43 @@ public class DialogueManager : MonoBehaviour
             }
         }
     }
+    void LateUpdate()
+    {
+        UpdateDialogueBox();
+    }
+    private void EnableDialogueControls()
+    {
+        controls.Player.Disable();
+        controls.UI.Enable();
+    }
+    private void DisableDialogueControls()
+    {
+        controls.Player.Enable();
+        controls.UI.Disable();
+    }
+    void Awake()
+    {
+        controls = new InputActions();
+        controls.UI.Continue.performed += OnContinuePressed;
+    }
     public void StartDialogue()
     {
         ongoing = true;
         currentNode = dialogue.GetEntryNode();
-        UpdateDialogueBox();
+        EnableDialogueControls();
     }
     private void EndDialogue()
     {
         ongoing = false;
         currentNode = null;
-        UpdateDialogueBox();
+        DisableDialogueControls();
+    }
+    private void OnContinuePressed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if(currentNode != null && currentNode.NodeType == "SimpleDialogueNode")
+        {
+            NextNode(currentNode.OutputPortList[0].Guid);
+        }
     }
     private void UpdateDialogueBox()
     {
@@ -87,9 +113,11 @@ public class DialogueManager : MonoBehaviour
             DeactivateOptions();
         }
         dialogueInterface.SetActive(ongoing);
+        optionBox.SetActive(optionsActive);
     }
     private void ActivateOptions()
     {
+        if(optionsActive) return;
         RectTransform box = optionBox.GetComponent<RectTransform>();
         box.sizeDelta = new Vector2(box.sizeDelta.x, currentNode.OutputPortList.Count() * 50);
         for (var i = 0; i < currentNode.OutputPortList.Count(); i++)
@@ -100,21 +128,22 @@ public class DialogueManager : MonoBehaviour
             option.GetComponent<OptionButton>().portGuid = port.Guid;
             option.transform.localPosition = new Vector3(0, 20 - 40 * i);
             optionList.Add(option);
-            optionBox.SetActive(true);
         }
         if (optionList.Count() > 0)
         {
             optionList[0].GetComponent<Button>().Select();
         }
+        optionsActive = true;
     }
     private void DeactivateOptions()
     {
+        if(!optionsActive) return;
         foreach (GameObject option in optionList)
         {
             Destroy(option);
         }
         optionList = new List<GameObject>();
-        optionBox.SetActive(false);
+        optionsActive = false;
     }
     private bool ValidateNodeCondition(DialogueNodeData node)
     {
@@ -142,20 +171,16 @@ public class DialogueManager : MonoBehaviour
         }
         return true;
     }
-
     private bool VaildateVariableCondition(string varName)
     {
-        if(varName == "true")
-            return true;
-        return false;
+        // Custom logics for validating game variables
+        return true;
     }
-
     public void NextNode(string portGuid)
     {
         if (currentNode.NodeType == "EndNode") return;
         NodeLinkData chosenLink = dialogue.NodeLinks.Find(l => l.BasePortGuid == portGuid);
         currentNode = dialogue.DialogueNodeData.Find(n => n.Guid == chosenLink.TargetNodeGuid);
-        UpdateDialogueBox();
         return;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private InputActions controls;
     private DialogueManager dialogueManager;
+    void OnEnable() {
+        controls.Player.Enable();
+        controls.UI.Disable();
+    }
+    void OnDisable() {
+        controls.Player.Disable();
+        controls.UI.Disable();
+    }
     void Start()
     {
         dialogueManager = GameObject.Find("Dialogue Manager").GetComponent<DialogueManager>();
     }
-    void LateUpdate()
+    void Awake()
     {
-        if(!dialogueManager.Ongoing && Input.GetKeyUp(KeyCode.Space))
+        controls = new InputActions();
+        controls.Player.Interaction.performed += OnInteractionPressed;
+    }
+    private void OnInteractionPressed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (!dialogueManager.Ongoing)
         {
             dialogueManager.StartDialogue();
         }
